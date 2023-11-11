5d6073148b448a9f516f03aa707163f3a93f248b
YoussefAboud <youssefaboud44@gmail.com>
2023-11-11 11:10:22 +0200

    feat: patient select doctor and slot

--- front_end_project/src/Patient.js
+++ front_end_project/src/Patient.js
@@ -1,89 +1,94 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import './Patient.css';
 
 const Patient = () => {
   const [doctors, setDoctors] = useState([]);
-  const [selectedDoctor, setSelectedDoctor] = useState(null);
-  const [doctorSlots, setDoctorSlots] = useState([]);
-  const [selectedSlot, setSelectedSlot] = useState(null);
-  const { UUID } = useParams();
-
-
-  return(<h1>Patient</h1>);
-  
-  /*
+  const [slots, setSlots] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+  const [selectedDoctor, setSelectedDoctor] = useState('');
+  const [selectedSlot, setSelectedSlot] = useState('');
+  const { uuid } = useParams();
 
   useEffect(() => {
-    axios.get(`localhost:3000/getDoctors/${UUID}`)
+    // Fetch list of doctors
+    axios.get(`http://localhost:4000/getDoctors/${uuid}`)
       .then(response => {
         setDoctors(response.data);
       })
       .catch(error => {
         console.error('Error fetching doctors:', error);
       });
-  }, []);
 
-  const handleDoctorChange = (e) => {
-    const doctorId = e.target.value;
-    setSelectedDoctor(doctors.find(doctor => doctor.id === doctorId));
-    axios.get(`api to ger doctor slots`)
-      .then(response => {
-        setDoctorSlots(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching doctor slots:', error);
-      });
+  }, [uuid]);
+
+  const handleDoctorChange = (event) => {
+    setSelectedDoctor(event.target.value);
+    setSelectedSlot('');
+    const selectedDoctorObj = doctors.find(doctor => doctor.id === event.target.value);
+    setSlots(selectedDoctorObj?.slots || []);
+  };
+  
+  const handleSlotChange = (event) => {
+    setSelectedSlot(event.target.value);
   };
 
-  const handleSlotChange = (e) => {
-    const slotId = e.target.value;
-    setSelectedSlot(doctorSlots.find(slot => slot.id === slotId));
+  //make appointment
+  const handleAppointmentSubmit = () => {
+
   };
 
-  const handleAppointmentBooking = () => {
-    if (selectedDoctor && selectedSlot) {
+  //update appointment
+  const handleAppointmentUpdate = () => {
 
-      axios.post(`localhost:3000/addAppointment/${UUID}/${doctorId}/${slotId}`)
-      .then(response => {
-        // Handle success, maybe show a confirmation message
-        console.log('Appointment booked successfully:', response.data);
-      })
-      .catch(error => {
-        console.error('Error booking appointment:', error);
-      });
-    }
   };
+  
+
+  //delete appointment
+  const handleAppointmentCancel = () =>{
+
+  }
+
+
   return (
     <div>
-      <h2>Book an Appointment</h2>
-      <div>
-        <label>Select a Doctor:</label>
-        <select onChange={handleDoctorChange}>
-          <option value="" disabled selected>Select a doctor</option>
-          {doctors.map(doctor => (
-            <option key={doctor.id} value={doctor.id}>{doctor.name}</option>
-          ))}
-        </select>
-      </div>
+      <h2>Available Doctors</h2>
+      <select onChange={handleDoctorChange} value={selectedDoctor}>
+        <option value="">Select a doctor</option>
+        {doctors.map(doctor => (
+          <option key={doctor.id} value={doctor.id}>
+            {doctor.name}
+          </option>
+        ))}
+      </select>
+
+      <h2>Your Appointments</h2>
+      <ul>
+        {appointments.map(appointment => (
+          <li key={appointment.id}>
+            Doctor: {appointment.doctor_id}, Slot: {appointment.slot_id}
+            <button onClick={() => handleAppointmentUpdate(appointment.id)}>Update</button>
+            <button onClick={() => handleAppointmentCancel(appointment.id)}>Cancel</button>
+          </li>
+        ))}
+      </ul>
+
       {selectedDoctor && (
         <div>
-          <label>Select a Slot:</label>
-          <select onChange={handleSlotChange}>
-            <option value="" disabled selected>Select a slot</option>
-            {doctorSlots.map(slot => (
-              <option key={slot.id} value={slot.id}>{slot.date} - {slot.hour}</option>
+          <h2>Slots for Selected Doctor</h2>
+          <select onChange={handleSlotChange} value={selectedSlot}>
+            <option value="">Select a slot</option>
+            {doctors.find(doctor => doctor.id === selectedDoctor)?.slots.map(slot => (
+              <option key={slot.id} value={slot.id}>
+                {slot.time}
+              </option>
             ))}
           </select>
+          <button onClick={handleAppointmentSubmit}>Make Appointment</button>
         </div>
       )}
-      <button onClick={handleAppointmentBooking} disabled={!selectedDoctor || !selectedSlot}>
-        Book Appointment
-      </button>
     </div>
   );
-  */
-}
+};
 
 export default Patient;
