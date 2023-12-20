0decdcfdd93c0446137cd0c6316459910c32acd5
Hazim Emam <84563544+HazimEmam@users.noreply.github.com>
2023-12-20 22:42:53 +0200

    Fix Env

--- front_end_project/src/apiConfig.js
+++ front_end_project/src/apiConfig.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.REACT_APP_FRONTEND_BASE_URL;
 const API_ENDPOINTS = {
     // patient
     PATIENT_SIGN_IN: `${BASE_URL}/patientSignIn`,
